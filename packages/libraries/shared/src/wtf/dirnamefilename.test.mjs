a11cc79026ea743d5b0acc0ea5d7a87b423295e4
noahehall <noahehall@users.noreply.github.com>
2021-12-26 09:43:37 -0500

    node 17.3.0 flowloader fails on packages/libraries/shared/pkg.json.script pnpm repo:test

--- packages/libraries/shared/src/wtf/dirnamefilename.test.mjs
+++ packages/libraries/shared/src/wtf/dirnamefilename.test.mjs
@@ -1,3 +1,7 @@
 import * as t from "@nodeproto/testproto";
 
-const test = t.suite("@nodeproto/shared");
+const test = t.suite("@nodeproto/shared: dirnamefilename.mjs");
+
+test("wtf node 17.3.0", () => {
+  test.assert(true === true);
+});
