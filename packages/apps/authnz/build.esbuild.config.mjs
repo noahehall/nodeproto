a72b8e4e8aad13fc6571bf698d81c64d49f60b7f
noahehall <noahehall@users.noreply.github.com>
2022-02-17 09:49:22 -0500

    working through esbuild integration with authnz server

--- packages/apps/authnz/build.esbuild.config.mjs
+++ packages/apps/authnz/build.esbuild.config.mjs
@@ -8,17 +8,27 @@ import {
 } from '@nodeproto/buildproto';
 
 import { resolve } from '@nodeproto/wtf';
+import { throwIt } from '@nodeproto/shared';
 
 // frigging eslint-flowtype-errors doesnt like top-level await
 (async () => {
+  const indir = await resolve('./src/routers');
+  const outdir = await resolve('./dist');
+
+  if (!indir || !outdir) return throwIt(`both indir & outdir reuqired in popcopy`);
+
+  const entry = await resolve('./src/root.mjs');
+
+  if (!entry) return throwIt(`entry required in to build esbuild config`);
+
   const popCopyConfig = esbuildPluginPopCopyConfig({
     endingWith: /openapi\.(yml|yaml)$/,
-    indir: await resolve('./src/api/routes'),
-    outdir: await resolve('./dist'),
+    indir,
+    outdir,
   });
 
   const configOpts = {
-    entry: await resolve('./src/root.mjs'),
+    entry,
     plugins: [esbuildPluginPopCopy(popCopyConfig)],
   };
 
